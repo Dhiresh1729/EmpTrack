72750815007042cc7ab6026b803b78c1640c34f1
Singh, Dhiresh X <dhiresh.x.singh@questdiagnostics.com>
2025-04-18 22:54:19 +0530

    created serializers and views

---
+++ backend/app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from app.models import Employee, Performance
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Performance
+        fields = '__all__'
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from app.models import Employee, Performance
+from app.serializers import EmployeeSerializer, PerformanceSerializer
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['department', 'position']
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['employee', 'review_date']
