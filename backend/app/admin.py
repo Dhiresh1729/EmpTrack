2aa537edf27ea97cddf06c2f2ad0add742dbfc7b
Singh, Dhiresh X <dhiresh.x.singh@questdiagnostics.com>
2025-04-18 21:28:12 +0530

    registered all models and created migrationsfor them

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employee, Attendance, Performance, Department, Project
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Attendance)
+admin.site.register(Performance)
+admin.site.register(Department)
+admin.site.register(Project)
